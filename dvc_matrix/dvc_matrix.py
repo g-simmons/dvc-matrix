dffeeea3bcb7d88a99bf191b4330b0c0456e66bb
Gabriel Simmons <simmons.gabe@gmail.com>
2023-06-05 00:45:53 -0700

    refactor, add interactivity to dvc-matrix run command

--- dvc_matrix/dvc_matrix.py
+++ dvc_matrix/dvc_matrix.py
@@ -8,6 +8,8 @@ import json as jsonlib
 import re
 from rich.console import Console
 from rich.table import Table
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import FuzzyWordCompleter
 
 
 def named_product(**items):
@@ -55,6 +57,130 @@ def print_stage_list(stage_list):
 
 
 @cli.command(
+    name="run",
+)
+@click.pass_context
+def run(ctx):
+    dvcyaml, dvcmatrix, dvclock = load_dvc_files()
+
+    stage_list = dvcmatrix["stages"]
+
+    completer = FuzzyWordCompleter(stage_list)
+    stage_name = prompt(
+        "Select stage to run: ",
+        completer=completer,
+    )
+
+    stage_list = get_stage_list(dvcyaml, dvcmatrix, dvclock, stage_name)
+    print_stage_list(stage_list)
+
+    # select from the stage list using fuzzy search
+
+    def get_stage_key(stage):
+        return " | ".join([f"{key}: {value}" for key, value in stage.items()])
+
+    meta_dict = {get_stage_key(stage): str(stage) for stage in stage_list}
+
+    # completer = FuzzyWordCompleter(words=meta_dict.keys(), meta_dict=meta_dict)
+    completer = FuzzyWordCompleter(
+        words=[s["cmd"] for s in stage_list],
+        meta_dict={s["cmd"]: get_stage_key(s) for s in stage_list},
+    )
+
+    cmd = prompt(
+        "Select stage to run: ",
+        completer=completer,
+    )
+    # paste the selected command into the terminal and exit, so that the user can run it
+    print(cmd)
+
+
+def get_status():
+    # capture the output of "dvc status --json" CLI command
+    status = os.popen("dvc status --json").read()
+
+    status = os.popen("dvc status --json").read()
+    status = jsonlib.loads(status)
+    return status
+
+
+# get the contents of dvc.lock
+def get_lock_stages(stage_name, lock):
+    lock_stages = {
+        lock_name: lock_stage
+        for lock_name, lock_stage in lock["stages"].items()
+        if stage_name in lock_name
+    }
+    return lock_stages
+
+
+def load_dvc_files():
+    dvcyaml = yaml.safe_load(open("dvc.yaml", "r"))
+    matrix = yaml.safe_load(open("dvc-matrix.yaml", "r"))
+    lock = yaml.safe_load(open("dvc.lock", "r"))
+    return dvcyaml, matrix, lock
+
+
+def get_stages():
+    stages_output = os.popen("dvc stage list --all").read()
+    stages_output = stages_output.split("\n")
+    stages_output = [s.split(" ") for s in stages_output if s]
+    stages = {s[0]: s[3] for s in stages_output}
+    return stages
+
+
+def get_stage_list_from_matrix(stage_name, matrix):
+    params = matrix["stages"][stage_name]["foreach-matrix"]
+    param_combinations = named_product(**params)
+    stage_list = []
+    for i, combination in enumerate(param_combinations):
+        stage_dict = {}
+        stage_dict["stage_name"] = f"{stage_name}@{i}"
+        stage_dict.update(combination)
+        stage_dict["status"] = "not run"
+
+        stage_list.append(stage_dict)
+
+    return stage_list
+
+
+def get_stage_list_from_lock(lock_stages, yaml, status):
+    stage_list = []
+    for stagename, lock_stage in lock_stages.items():
+        formatted_command = lock_stage["cmd"]
+        dvc_stage = yaml["stages"][stagename.split("@")[0]]
+        if "do" not in dvc_stage:
+            print(f"Stage {stagename} has no do section")
+            continue
+        command_template = dvc_stage["do"]["cmd"]
+        params = unformat(command_template, formatted_command)
+        stage_dict = {}
+        stage_dict.update(params)
+        stage_dict["stage_name"] = stagename
+        stage_dict["cmd"] = formatted_command
+
+        try:
+            status[stagename]
+            stage_dict["status"] = "changed"
+        except KeyError:
+            stage_dict["status"] = "ok"
+
+        stage_list.append(stage_dict)
+
+    return stage_list
+
+
+def get_stage_list(yaml, matrix, lock, stage_name):
+    lock_stages = get_lock_stages(stage_name, lock)
+    status = get_status()
+
+    if len(lock_stages) == 0:
+        return get_stage_list_from_matrix(stage_name, matrix)
+    else:
+        return get_stage_list_from_lock(lock_stages, yaml, status)
+
+
+@cli.command(
     name="status",
 )
 @click.option("--json", is_flag=True, help="Print status list as JSON")
@@ -63,64 +189,15 @@ def status(ctx, json):
     """
     This function displays the status of each combination of parameters in a foreach-matrix, using the output from dvc status CLI.
     """
+    status = get_status()
 
-    status = os.popen("dvc status --json").read()
-    status = jsonlib.loads(status)
-
-    dvcyaml = yaml.safe_load(open("dvc.yaml", "r"))
-    matrix = yaml.safe_load(open("dvc-matrix.yaml", "r"))
-    lock = yaml.safe_load(open("dvc.lock", "r"))
+    dvcyaml, matrix, lock = load_dvc_files()
 
     stage_lists = []
+    # for stagename, lock_stage in lock["stages"].items():
     for yaml_name in dvcyaml["stages"]:
-
-        lock_stages = {
-            lock_name: lock_stage
-            for lock_name, lock_stage in lock["stages"].items()
-            if yaml_name in lock_name
-        }
-
-        if len(lock_stages) == 0:
-            print(f"Stage {yaml_name} not found in dvc.lock")
-            params = matrix["stages"][yaml_name]["foreach-matrix"]
-            param_combinations = named_product(**params)
-            stage_list = []
-            for i, combination in enumerate(param_combinations):
-                stage_dict = {}
-                stage_dict["stage_name"] = f"{yaml_name}@{i}"
-                stage_dict.update(combination)
-                stage_dict["status"] = "not run"
-
-                stage_list.append(stage_dict)
-
-            stage_lists.append(stage_list)
-
-        else:
-            stage_list = []
-            for stagename, lock_stage in lock_stages.items():
-                dvc_stage = dvcyaml["stages"][stagename.split("@")[0]]
-                if "do" not in dvc_stage:
-                    print(f"Stage {stagename} has no do section")
-                    continue
-
-                # get stage parameters
-                formatted_out = lock_stage["outs"][0]["path"]
-                out_template = dvc_stage["do"]["outs"][0]
-                params = unformat(out_template, formatted_out)
-
-                stage_dict = {}
-                stage_dict["stage_name"] = stagename
-                stage_dict.update(params)
-
-                if stagename not in status:
-                    stage_dict["status"] = "ok"
-                elif "changed command" in status[stagename]:
-                    stage_dict["status"] = "changed command"
-                else:
-                    stage_dict["status"] = ", ".join([key for key in ["changed outs", "changed deps"] if key in status[stagename]])
-
-                stage_list.append(stage_dict)
-
+        stage_list = get_stage_list(dvcyaml, matrix, lock, yaml_name)
+        if stage_list:
             stage_lists.append(stage_list)
 
     if json:
